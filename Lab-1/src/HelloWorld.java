9afdb3a66f6562a81b672d47ebd5d8dc252a8cf1
git config --global user.name "Caleb Ferch <git config --global user.email caf44@students.uwf.edu>
2020-08-30 14:45:58 -0500

    Working basic Solution

---
+++ Lab-1/src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+	public static void main (String[] argv)
+	{
+		System.out.println("Hello Caleb Ferch caf44@students.uwf.edu");
+		for (int i = 1; i <=100; ++i)
+		{
+			System.out.print(" "+i);
+		}
+	}
+
+}
